9c920dad6e4911ec07c5e8e5106ee6be405b65b4
Brahmleen Papneja <152105520+PapnejaPrints@users.noreply.github.com>
2026-03-20 14:29:06 -0400

    updated live birth counter

---
+++ bili-app/src/i18n.js
@@ -0,0 +1,40 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslation from './locales/en/translation.json';
+import frTranslation from './locales/fr/translation.json';
+
+const resources = {
+  en: {
+    translation: enTranslation
+  },
+  fr: {
+    translation: frTranslation
+  }
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'fr'],
+    
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+      lookupLocalStorage: 'i18nextLng'
+    },
+    
+    interpolation: {
+      escapeValue: false
+    },
+    
+    react: {
+      useSuspense: false
+    }
+  });
+
+export default i18n;
